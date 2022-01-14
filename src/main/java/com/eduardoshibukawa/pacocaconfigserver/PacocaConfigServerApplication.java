644cae1cbb9de2a8d49e13e5421c6a2727b38fc4
Eduardo Shibukawa <eduardoshibuka@gmail.com>
2022-01-14 11:40:10 -0300

    inital project setup

---
+++ src/main/java/com/eduardoshibukawa/pacocaconfigserver/PacocaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.eduardoshibukawa.pacocaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacocaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacocaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardoshibukawa/pacocaconfigserver/PacocaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoshibukawa.pacocaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacocaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
